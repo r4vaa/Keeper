09faa7397f02c88d908db96287fa9a83dec6ef6e
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-04-15 20:07:21 +0530

    use of map function

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-function Note(){
+
+function Note(props){
     return (
         <div className="note">
-            <h1>This is the title</h1>
-            <p>This is my Content</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     )
 }

11ccfa836c5105f8ef79b09623ea13ac8867eb6b
Dheeraj <42409791+r4vaa@users.noreply.github.com>
2023-04-26 01:27:33 +0530

    add functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
-
+import CreateArea from "./CreateArea";
 
 function App() {
+
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote){
+    setNotes(prevNotes => {
+       return [...prevNotes, newNote];
+    })
+  }
+
   return (
-    <div className="App">
-     <Header />
-      {notes.map((noteItem) => 
-        <Note 
-            key = {noteItem.key}
-            title = {noteItem.title}
-            content = {noteItem.content}
-        />
-      )}
-     <Footer />
+    <div>
+      <Header />
+      <CreateArea  onAdd ={addNote}/>
+      {
+        notes.map(noteItem => {
+            return <Note title={noteItem.title} content= {noteItem.content} />
+        })
+      }
+      <Footer />
     </div>
   );
 }
